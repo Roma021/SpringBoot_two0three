d28fb971731db3283f3e64f7a749047d6a1d2667
Roma021 <dtmogh@gmail.com>
2019-05-20 14:16:40 -0400

    twopointthree

---
+++ src/main/java/com/example/demo/SpringBootTwo0threeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTwo0threeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTwo0threeApplication.class, args);
+	}
+
+}
